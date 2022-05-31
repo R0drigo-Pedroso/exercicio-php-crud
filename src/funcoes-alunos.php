720ee84c6a794c6488bab3926647cbaceca1221f
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-05-31 11:51:29 -0300

    atualização

--- src/funcoes-alunos.php
+++ src/funcoes-alunos.php
@@ -3,13 +3,13 @@
 require_once "conexao.php";
 
 // Inserir dado de Alunos
-function inserirAluno(PDO $cenexao, int $id, string $nome, float $primeiraNota, float $segundaNota, float $media, string $situacao):void {
-    $sql = "INSERT INTO alunos (id, nome, primeiraNota, segundaNota, media, situacao) VALUES (:id, :nome, :primeira_Nota, :segunda_Nota, :media, :situacao)";
+function inserirAluno(PDO $cenexao, string $nome, float $primeiraNota, float $segundaNota, float $media, string $situacao):void {
+    $sql = "INSERT INTO alunos (nome, primeiraNota, segundaNota, media, situacao) VALUES (:nome, :primeira_Nota, :segunda_Nota, :media, :situacao)";
     
     try {
         $consulta = $cenexao->prepare($sql);
 
-        $consulta->bindParam(":id", $id, PDO::PARAM_INT);
+        // $consulta->bindParam(":id", $id, PDO::PARAM_INT);
         $consulta->bindParam(":nome", $nome, PDO::PARAM_STR);
         $consulta->bindParam(":primeira_Nota", $primeiraNota, PDO::PARAM_STR);
         $consulta->bindParam(":segunda_Nota", $segundaNota, PDO::PARAM_STR);
@@ -18,6 +18,25 @@ function inserirAluno(PDO $cenexao, int $id, string $nome, float $primeiraNota, 
         
         $consulta->execute();
 
+    } catch (Exception $error) {
+        die ("Erro: ".$error->getMessage());
+    }
+}
+
+function lerAlunos(PDO $cenexao):array {
+    $sql = "SELECT * FROM alunos";
+
+    try {
+        $consulta = $cenexao->prepare($sql);
+        $consulta->execute();
+
+        $alunos = [];
+
+        while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+            $alunos[] = $linha;
+        }
+
+        return $alunos;
     } catch (Exception $error) {
         die ("Erro: ".$error->getMessage());
     }
